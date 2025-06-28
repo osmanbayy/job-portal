cafa64bba379e1483bfce94a97e95977bd22832e
Osman Bay <bayosman342@gmail.com>
2025-06-29 02:15:56 +0300

    Add Clerk authentication integration and update Navbar for user state management

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import { assets } from "../assets/assets";
 import { ChevronDown, LogIn, ShieldCheck } from "lucide-react";
 import { JobCategories } from "../assets/assets";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const { openSignIn } = useClerk();
+  const { user } = useUser();
+
   return (
     <div className="navbar">
       <div className="flex items-center justify-between px-4 sm:px-6 lg:px-8">
@@ -60,14 +65,32 @@ const Navbar = () => {
           </a>
         </div>
 
-        <div className="flex items-center gap-2 sm:gap-4 text-sm sm:text-base">
-          <button className="max-md:hidden flex items-center gap-3 text-gray-300 whitespace-nowrap px-4 sm:px-6 py-1.5 sm:py-2 rounded-full border border-gray-700 hover:bg-gray-800 hover:text-white transition-all cursor-pointer">
-            Recruiter Login <ShieldCheck className="size-4" />
-          </button>
-          <button className="flex items-center gap-3 bg-gray-800 hover:bg-transparent border border-transparent cursor-pointer hover:border-gray-700 transition-all text-white px-4 sm:px-6 py-1.5 sm:py-2 rounded-full whitespace-nowrap">
-            Login <LogIn className="size-4" />
-          </button>
-        </div>
+        {user ? (
+          <div className="text-white/80 flex items-center gap-2 sm:gap-4 text-sm sm:text-base">
+            <Link
+              to="/applications"
+              className="flex items-center gap-2 sm:gap-4 text-sm sm:text-base invisible md:visible"
+            >
+              {" "}
+              Applied Jobs
+            </Link>
+            <p className="invisible md:visible">|</p>
+            <p>Hi! {user.firstName}</p>
+            <UserButton />
+          </div>
+        ) : (
+          <div className="flex items-center gap-2 sm:gap-4 text-sm sm:text-base">
+            <button className="max-md:hidden flex items-center gap-3 text-gray-300 whitespace-nowrap px-4 sm:px-6 py-1.5 sm:py-2 rounded-full border border-gray-700 hover:bg-gray-800 hover:text-white transition-all cursor-pointer">
+              Recruiter Login <ShieldCheck className="size-4" />
+            </button>
+            <button
+              onClick={() => openSignIn()}
+              className="flex items-center gap-3 bg-gray-800 hover:bg-transparent border border-transparent cursor-pointer hover:border-gray-700 transition-all text-white px-4 sm:px-6 py-1.5 sm:py-2 rounded-full whitespace-nowrap"
+            >
+              Login <LogIn className="size-4" />
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
