437a06c6d6fb9c5224f77c11498d2863150261ba
Osman Bay <bayosman342@gmail.com>
2025-07-07 00:25:34 +0300

    fix: update MongoDB connection URI to remove specific database path

--- server/config/database.js
+++ server/config/database.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 const connectDatabase = async () => {
   mongoose.connection.on('connected', () => console.log("Database Connected Successfully!"));
 
-  await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`);
+  await mongoose.connect(`${process.env.MONGODB_URI}`);
 }
 
 export default connectDatabase;
